eeef29148898d8834a203ee9dcc3d55785bc6b0d
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-12-25 22:58:02 -0800

    Create project

---
+++ KamiLib.cs
@@ -0,0 +1,24 @@
+﻿using Dalamud.Game.ClientState;
+using Dalamud.Game.Gui;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using DalamudCommandManager = Dalamud.Game.Command.CommandManager;
+
+namespace KamiLib;
+
+public class Service
+{
+    [PluginService] public static DalamudPluginInterface PluginInterface { get; private set; } = null!;
+    [PluginService] public static DalamudCommandManager Commands { get; private set; } = null!;
+    [PluginService] public static ClientState ClientState { get; private set; } = null!;
+    [PluginService] public static ChatGui Chat { get; private set; } = null!;
+    [PluginService] public static GameGui GameGui { get; private set; } = null!;
+}
+
+public static class KamiLib
+{
+    public static void Initialize(DalamudPluginInterface pluginInterface)
+    {
+        pluginInterface.Create<Service>();
+    }
+}
