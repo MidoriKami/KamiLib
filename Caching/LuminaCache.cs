ee248143d54b218c7a6de48df112462b7c3a0132
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-12-26 13:12:27 -0800

    Add Caching helper

---
+++ Caching/LuminaCache.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Lumina.Excel;
+
+namespace KamiLib.Caching;
+
+public class LuminaCache<T> where T : ExcelRow
+{
+    private readonly Func<uint, T> searchAction;
+
+    public LuminaCache(Func<uint, T>? action = null)
+    {
+        searchAction = action ?? (row => Service.DataManager.GetExcelSheet<T>()!.GetRow(row)!);
+    }
+
+    private readonly Dictionary<uint, T> cache = new();
+
+    public T GetRow(uint id)
+    {
+        if (cache.TryGetValue(id, out var value))
+        {
+            return value;
+        }
+        else
+        {
+            return cache[id] = searchAction(id);
+        }
+    }
+}
