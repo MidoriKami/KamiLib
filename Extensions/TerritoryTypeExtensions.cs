1d674f7851945566544300865e39245b48e6ac31
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-12-27 04:17:32 -0800

    Add TerritoryTypeExtensions.cs
    Draws a faded rowID with a regular placename text field

---
+++ Extensions/TerritoryTypeExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Dalamud.Interface;
+using Dalamud.Utility;
+using ImGuiNET;
+using KamiLib.Caching;
+using KamiLib.Utilities;
+using Lumina.Excel.GeneratedSheets;
+
+namespace KamiLib.Extensions;
+
+public static class TerritoryTypeExtensions
+{
+    public static void DrawLabel(this TerritoryType data)
+    {
+        var placeString = data.GetLabel();
+        
+        var startPosition = ImGui.GetCursorPos();
+        ImGui.TextColored(Colors.Grey, data.RowId.ToString());
+        ImGui.SameLine(startPosition.X + 50.0f * ImGuiHelpers.GlobalScale);
+        ImGui.Text(placeString);
+    }
+
+    public static string GetLabel(this TerritoryType data)
+    {
+        var placeNameRow = data.PlaceName.Row;
+        var placeName = PlaceNameCache.Instance.GetRow(placeNameRow);
+        var placeString = placeName?.Name.ToDalamudString().TextValue ?? "Unknown PlaceName";
+
+        return placeString;
+    }
+}
