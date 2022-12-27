8f4aa878eafb9a9444a7f1b267afa64419512316
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-12-27 04:17:51 -0800

    Add SearchResult helper for TerritoryTypes

---
+++ BlacklistSystem/SearchResult.cs
@@ -0,0 +1,11 @@
+﻿using Dalamud.Utility;
+using KamiLib.Caching;
+
+namespace KamiLib.BlacklistSystem;
+
+public class SearchResult
+{
+    public uint TerritoryID { get; init; }
+    private uint PlaceNameRow => TerritoryTypeCache.Instance.GetRow(TerritoryID)?.PlaceName.Row ?? 0;
+    public string TerritoryName => PlaceNameCache.Instance.GetRow(PlaceNameRow)?.Name.ToDalamudString().TextValue ?? "Unknown PlaceName Row";
+}
