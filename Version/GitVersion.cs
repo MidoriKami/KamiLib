4da7ace0c3575866b522e9abdc1f6f7ada8d775b
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-12-27 14:20:45 -0800

    Add GitVersioning utility

---
+++ Version/GitVersion.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace KamiLib.Version;
+
+public static class GitVersion
+{
+    public static string VersionString()
+    {
+        var assemblyInformation = Assembly.GetExecutingAssembly().FullName!.Split(',');
+        return assemblyInformation[1].Replace('=', ' ');
+    }
+}
