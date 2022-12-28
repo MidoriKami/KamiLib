2f77bd9cd4edb6a433e48a3254b14babaca72d1b
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-12-27 16:05:19 -0800

    Add hook safety helper

---
+++ ExceptionSafety/Safety.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Dalamud.Logging;
+
+namespace KamiLib.ExceptionSafety;
+
+public static class Safety
+{
+    public static void ExecuteSafe(Action action, string? message = null) 
+    {
+        try 
+        {
+            action();
+        } 
+        catch (Exception exception)
+        {
+            PluginLog.Error(exception, message ?? "Caught Exception Safely");
+        }
+    }
+}
