6d132a05bfb1299e32be0bc8191ebec4198a8f8a
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-12-27 22:44:54 -0800

    Use KamiLib.Caching

---
+++ Caching/ActionCache.cs
@@ -0,0 +1,15 @@
+﻿using Lumina.Excel.GeneratedSheets;
+
+namespace KamiLib.Caching;
+
+public class ActionCache : LuminaCache<Action>
+{
+    private static ActionCache? _instance;
+    public static ActionCache Instance => _instance ??= new ActionCache();
+
+    private ActionCache()
+    {
+        // Initialize Inherited Type
+    }
+
+}
