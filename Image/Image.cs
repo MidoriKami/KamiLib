b31564ccba45b0f5f8dc8943dd8fc46b9ca1683a
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-12-28 15:16:18 -0800

    Move Image to KamiLib

---
+++ Image/Image.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using ImGuiScene;
+
+namespace KamiLib.Image;
+
+public static class Image
+{
+    public static TextureWrap LoadImage(string imageName)
+    {
+        var assemblyLocation = Service.PluginInterface.AssemblyLocation.DirectoryName!;
+        var imagePath = Path.Combine(assemblyLocation, $@"images\{imageName}.png");
+
+        return Service.PluginInterface.UiBuilder.LoadImage(imagePath);
+    }
+}
