8d1263cb98878ab91249852876170d0cc259972a
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-12-29 15:34:31 -0800

    Move Common Window Drawflags to KamiLib

---
+++ Windows/DrawFlags.cs
@@ -0,0 +1,19 @@
+﻿using ImGuiNET;
+
+namespace KamiLib.Windows;
+
+public static class DrawFlags
+{
+    public const ImGuiWindowFlags AutoResize = ImGuiWindowFlags.NoFocusOnAppearing |
+                                               ImGuiWindowFlags.NoTitleBar |
+                                               ImGuiWindowFlags.NoScrollbar |
+                                               ImGuiWindowFlags.NoCollapse |
+                                               ImGuiWindowFlags.AlwaysAutoResize;
+
+    public const ImGuiWindowFlags ManualSize = ImGuiWindowFlags.NoFocusOnAppearing |
+                                               ImGuiWindowFlags.NoTitleBar |
+                                               ImGuiWindowFlags.NoCollapse;
+
+    public const ImGuiWindowFlags LockPosition = ImGuiWindowFlags.NoMove |
+                                                 ImGuiWindowFlags.NoResize;
+}
