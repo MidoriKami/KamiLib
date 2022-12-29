ba65089dcec74b37c81850bc98a8ffe0782f2c13
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-12-29 02:07:23 -0800

    Add DalamudLinkPayLoad overload

--- Utilities/Chat.cs
+++ Utilities/Chat.cs
@@ -1,14 +1,12 @@
 ﻿using Dalamud.Game.Text.SeStringHandling;
+using Dalamud.Game.Text.SeStringHandling.Payloads;
 
 namespace KamiLib.Utilities;
 
 public static class Chat
 {
-    public static void Print(string tag, string message)
-    {
-        Service.Chat.Print(GetBaseString(tag, message).BuiltString);
-    }
-    
+    public static void Print(string tag, string message) => Service.Chat.Print(GetBaseString(tag, message).BuiltString);
+
     public static void PrintHelp(string command, string? helpText = null)
     {
         var message = GetBaseString("Command", command);
@@ -20,17 +18,37 @@ public static class Chat
 
         Service.Chat.Print(message.BuiltString);
     }
-
-    public static void PrintError(string message)
-    {
-        Service.Chat.PrintError(GetBaseString("Error", message).BuiltString);
-    }
     
-    private static SeStringBuilder GetBaseString(string tag, string message)
+    public static void Print(string tag, string message, DalamudLinkPayload? payload)
     {
-        return new SeStringBuilder()
-            .AddUiForeground($"[{KamiCommon.PluginName}] ", 45)
-            .AddUiForeground($"[{tag}] ", 62)
-            .AddText(message);
+        if (payload is null)
+        {
+            Print(tag, message);
+            return;
+        }
+        
+        Service.Chat.Print(GetBaseString(tag, message, payload).BuiltString);
+    }
+
+    public static void PrintError(string message) => Service.Chat.PrintError(GetBaseString("Error", message).BuiltString);
+    
+    private static SeStringBuilder GetBaseString(string tag, string message, DalamudLinkPayload? payload = null)
+    {
+        if (payload is null)
+        {
+            return new SeStringBuilder()
+                .AddUiForeground($"[{KamiCommon.PluginName}] ", 45)
+                .AddUiForeground($"[{tag}] ", 62)
+                .AddText(message);
+        }
+        else
+        {
+            return new SeStringBuilder()
+                .AddUiForeground($"[{KamiCommon.PluginName}] ", 45)
+                .AddUiForeground($"[{tag}] ", 62)
+                .Add(payload)
+                .AddUiForeground(message, 35)
+                .Add(RawPayload.LinkTerminator);
+        }
     }
 }
