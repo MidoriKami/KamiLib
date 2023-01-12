664cc3bbaa751e208b8621200a0875522e0e5324
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-01-12 04:41:19 -0800

    Add Address Logging functions

---
+++ Misc/Logging.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Dalamud.Logging;
+
+namespace KamiLib.Misc;
+
+public static unsafe class Logging
+{
+    public static void PrintAddress(void* address, string label)
+    {
+        PrintAddress(new nint(address), label);
+    }
+    
+    public static void PrintAddress(nint address, string label)
+    {
+        PluginLog.Debug($"{GetAddressString(address)} - {label}");
+    }
+
+    private static string GetAddressString(nint address)
+    {
+        if (Process.GetCurrentProcess().MainModule is { } mainModule)
+        {
+            var begin = mainModule.BaseAddress.ToInt64();
+            var end = begin + mainModule.ModuleMemorySize;
+
+            if (address > 0 && address >= begin && address <= end)
+            {
+                return $"ffxiv_dx11.exe+{(address - begin):X}";
+            }
+            else
+            {
+                return $"{address:X}";
+            }
+        }
+
+        return "Unable To Get Module Info";
+    }
+}
