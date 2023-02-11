b40e531f894b7432944dc27f7ee5d600cfb96a54
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-02-11 10:30:21 -0800

    Add Simpler Hooking Functions for Addons

---
+++ Hooking/AddonHook.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Dalamud.Hooking;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace KamiLib.Hooking;
+
+public static unsafe class AddonHook
+{
+    public static Hook<T> Hook<T>(nint addon, uint vFuncIndex, T function) where T : Delegate => Dalamud.Hooking.Hook<T>.FromAddress(new nint(((AtkUnitBase*)addon)->AtkEventListener.vfunc[vFuncIndex]), function);
+    public static Hook<T> Hook<T>(void* addon, uint vFuncIndex, T function) where T : Delegate => Dalamud.Hooking.Hook<T>.FromAddress(new nint(((AtkUnitBase*)addon)->AtkEventListener.vfunc[vFuncIndex]), function);
+}
