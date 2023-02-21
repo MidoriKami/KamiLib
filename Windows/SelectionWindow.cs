56e61b80d84e1ca6bcfdf5bcf8e87d9a10544c5b
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-02-20 19:56:13 -0800

    Use Tables for Selection Window

--- Windows/SelectionWindow.cs
+++ Windows/SelectionWindow.cs
@@ -3,14 +3,12 @@ using System.Numerics;
 using Dalamud.Interface;
 using Dalamud.Interface.Windowing;
 using ImGuiNET;
-using KamiLib.Drawing;
 using KamiLib.Interfaces;
 
 namespace KamiLib.Windows;
 
 public abstract class SelectionWindow : Window, IDrawable
 {
-    private readonly float horizontalWeight;
     private readonly float verticalHeight;
     private const bool ShowBorders = false;
 
@@ -19,9 +17,8 @@ public abstract class SelectionWindow : Window, IDrawable
     protected abstract IEnumerable<ISelectable> GetSelectables();
     protected bool ShowScrollBar = true;
 
-    protected SelectionWindow(string windowName, float xPercent, float height) : base(windowName)
+    protected SelectionWindow(string windowName, float height = 0.0f) : base(windowName)
     {
-        horizontalWeight = xPercent;
         verticalHeight = height;
     }
 
@@ -30,57 +27,42 @@ public abstract class SelectionWindow : Window, IDrawable
         var region = ImGui.GetContentRegionAvail();
         var itemSpacing = ImGui.GetStyle().ItemSpacing;
 
-        var leftSideWidth = region.X * horizontalWeight - itemSpacing.X / 2.0f;
         var topLeftSideHeight = region.Y - verticalHeight * ImGuiHelpers.GlobalScale - itemSpacing.Y / 2.0f;
 
-        if(ImGui.BeginChild($"###{KamiCommon.PluginName}LeftSide", new Vector2( leftSideWidth, topLeftSideHeight), ShowBorders, ImGuiWindowFlags.NoDecoration))
+        if (ImGui.BeginTable($"{KamiCommon.PluginName}TableContainer", 2, ImGuiTableFlags.Resizable))
         {
-            selectionList.Draw(GetSelectables());
-        }
-        ImGui.EndChild();
+            ImGui.TableSetupColumn("##LeftColumn", ImGuiTableColumnFlags.None, 2);
+            ImGui.TableSetupColumn("##RightColumn", ImGuiTableColumnFlags.None, 4);
+            
+            ImGui.TableNextColumn();
 
-        var bottomLeftChildPosition = ImGui.GetCursorPos();
-        
-        ImGui.SameLine();
-        DrawVerticalLine();
+            var regionSize = ImGui.GetContentRegionAvail();
+            
+            if(ImGui.BeginChild($"###{KamiCommon.PluginName}LeftSide", regionSize with { Y = topLeftSideHeight }, ShowBorders, ImGuiWindowFlags.NoDecoration))
+            {
+                selectionList.Draw(GetSelectables());
+            }
+            ImGui.EndChild();
 
-        var rightSideWidth = region.X * (1.0f - horizontalWeight) - itemSpacing.X / 2.0f;
-        
-        if(ImGui.BeginChild($"###{KamiCommon.PluginName}RightSide", new Vector2(rightSideWidth, 0), ShowBorders, (ShowScrollBar ? ImGuiWindowFlags.AlwaysVerticalScrollbar : ImGuiWindowFlags.None) | ImGuiWindowFlags.NoDecoration))
-        {
-            selectionList.DrawSelected();
+            if(ImGui.BeginChild($"###{KamiCommon.PluginName}BottomLeftSide", regionSize with { Y = verticalHeight * ImGuiHelpers.GlobalScale }, ShowBorders, ImGuiWindowFlags.NoDecoration))
+            {
+                DrawExtras();
+            }
+            ImGui.EndChild();
+            
+            ImGui.TableNextColumn();
+            if(ImGui.BeginChild($"###{KamiCommon.PluginName}RightSide", Vector2.Zero, ShowBorders, (ShowScrollBar ? ImGuiWindowFlags.AlwaysVerticalScrollbar : ImGuiWindowFlags.None) | ImGuiWindowFlags.NoDecoration))
+            {
+                selectionList.DrawSelected();
+            }
+            ImGui.EndChild();
+            
+            ImGui.EndTable();
         }
-        ImGui.EndChild();
-        
-        ImGui.SetCursorPos(bottomLeftChildPosition);
-        
-        if(ImGui.BeginChild($"###{KamiCommon.PluginName}BottomLeftSide", new Vector2(leftSideWidth, verticalHeight * ImGuiHelpers.GlobalScale), ShowBorders, ImGuiWindowFlags.NoDecoration))
-        {
-            DrawExtras();
-        }
-        ImGui.EndChild();
         
         DrawSpecial();
     }
 
-    protected virtual void DrawExtras()
-    {
-        
-    }
-    
-    protected virtual void DrawSpecial()
-    {
-        
-    }
-
-    private static void DrawVerticalLine()
-    {
-        var contentArea = ImGui.GetContentRegionAvail();
-        var itemSpacing = ImGui.GetStyle().ItemSpacing;
-        var cursor = new Vector2(ImGui.GetCursorScreenPos().X - itemSpacing.X / 2.0f, ImGui.GetCursorScreenPos().Y );
-        var drawList = ImGui.GetWindowDrawList();
-        var color = ImGui.GetColorU32(Colors.White);
-
-        drawList.AddLine(cursor, cursor with {Y = cursor.Y + contentArea.Y}, color, 1.0f);
-    }
+    protected virtual void DrawExtras() { }
+    protected virtual void DrawSpecial() { }
 }
