730a6c90f7e05b115e5b6d80352e6f46902179ca
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-02-26 15:28:36 -0800

    Remove extra spacing

--- Interfaces/ISelectionWindowTab.cs
+++ Interfaces/ISelectionWindowTab.cs
@@ -5,9 +5,7 @@ namespace KamiLib.Interfaces;
 public interface ISelectionWindowTab
 {
     string TabName { get; }
-    
     ISelectable? LastSelection { get; set; }
-    
     IEnumerable<ISelectable> GetTabSelectables();
     void DrawTabExtras() { }
 }
