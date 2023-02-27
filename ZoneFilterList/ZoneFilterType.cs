39349ce6da17b2a6ba0284efb89180cf1fd983e0
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-02-27 14:44:20 -0800

    Use Object Initializer instead of ctor

--- ZoneFilterList/ZoneFilterType.cs
+++ ZoneFilterList/ZoneFilterType.cs
@@ -10,22 +10,29 @@ public enum ZoneFilterTypeId
 
 public class ZoneFilterType
 {
-    public static ZoneFilterType WhiteList { get; } = new(ZoneFilterTypeId.Whitelist, Strings.ZoneFilterList_ClearWhitelist,
-                                                          Strings.ZoneFilterList_CurrentlyWhitelisted,
-                                                          Strings.ZoneFilterList_EmptyWhitelist,
-                                                          Strings.ZoneFilterList_SelectZonesToWhitelist);
-
-    public static ZoneFilterType BlackList { get; } = new(ZoneFilterTypeId.Blacklist,
-                                                          Strings.ZoneFilterList_ClearBlacklist,
-                                                          Strings.ZoneFilterList_CurrentlyBlacklisted,
-                                                          Strings.ZoneFilterList_EmptyBlacklist,
-                                                          Strings.ZoneFilterList_SelectZonesToBlacklist);
-
-    public ZoneFilterTypeId Id { get; }
-    public string ClearButton { get; }
-    public string CurrentLabel { get; }
-    public string IsEmptyLabel { get; }
-    public string SelectZonesLabel { get; }
+    public static ZoneFilterType WhiteList { get; } = new()
+    {
+        Id = ZoneFilterTypeId.Whitelist,
+        ClearButton = Strings.ZoneFilterList_ClearWhitelist,
+        CurrentLabel = Strings.ZoneFilterList_CurrentlyWhitelisted,
+        IsEmptyLabel = Strings.ZoneFilterList_EmptyWhitelist,
+        SelectZonesLabel = Strings.ZoneFilterList_SelectZonesToWhitelist
+    };
+    
+    public static ZoneFilterType BlackList { get; } = new()
+    {
+        Id = ZoneFilterTypeId.Blacklist,
+        ClearButton = Strings.ZoneFilterList_ClearBlacklist,
+        CurrentLabel = Strings.ZoneFilterList_CurrentlyBlacklisted,
+        IsEmptyLabel = Strings.ZoneFilterList_EmptyBlacklist,
+        SelectZonesLabel = Strings.ZoneFilterList_SelectZonesToBlacklist
+    };
+    
+    public ZoneFilterTypeId Id { get; init; }
+    public string ClearButton { get; init; }
+    public string CurrentLabel { get; init; }
+    public string IsEmptyLabel { get; init; }
+    public string SelectZonesLabel { get; init; }
 
     public static ZoneFilterType? FromId(ZoneFilterTypeId id) => id switch
     {
@@ -33,14 +40,4 @@ public class ZoneFilterType
         ZoneFilterTypeId.Blacklist => BlackList,
         _ => null
     };
-
-    private ZoneFilterType(
-        ZoneFilterTypeId id, string clearButton, string currentLabel, string isEmptyLabel, string selectZonesLabel)
-    {
-        Id = id;
-        ClearButton = clearButton;
-        CurrentLabel = currentLabel;
-        IsEmptyLabel = isEmptyLabel;
-        SelectZonesLabel = selectZonesLabel;
-    }
 }
