fdd7e84bacc04f7c618bf64c9a699a7c819b1acf
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-02-26 16:02:03 -0800

    Account for framepadding

--- Windows/TabbedSelectionWindow.cs
+++ Windows/TabbedSelectionWindow.cs
@@ -52,14 +52,15 @@ public abstract class TabbedSelectionWindow : SelectionWindow
     protected override void DrawExtras()
     {
         var region = ImGui.GetContentRegionAvail();
+        var padding = ImGui.GetStyle().FramePadding;
         
-        if (ImGui.BeginChild("###TabbedWindowTabExtras", region with { Y = region.Y / 2.0f }))
+        if (ImGui.BeginChild("###TabbedWindowTabExtras", region with { Y = region.Y / 2.0f - padding.Y}))
         {
             selectedTab?.DrawTabExtras();
         }
         ImGui.EndChild();
 
-        if (ImGui.BeginChild("###TabbedWindowWindowExtras", region with { Y = region.Y / 2.0f }))
+        if (ImGui.BeginChild("###TabbedWindowWindowExtras", region with { Y = region.Y / 2.0f - padding.Y}))
         {
             DrawWindowExtras();
         }
