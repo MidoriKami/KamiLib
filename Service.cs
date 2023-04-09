ba2716ee50ca45d2c8cfe7078e605d533933ffd7
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-04-08 23:50:52 -0700

    Remove unused services

--- Service.cs
+++ Service.cs
@@ -1,10 +1,6 @@
 ﻿using Dalamud.Data;
-using Dalamud.Game;
 using Dalamud.Game.ClientState;
-using Dalamud.Game.ClientState.Aetherytes;
-using Dalamud.Game.ClientState.Objects;
 using Dalamud.Game.Gui;
-using Dalamud.Game.Gui.Toast;
 using Dalamud.IoC;
 using Dalamud.Plugin;
 using DalamudCommandManager = Dalamud.Game.Command.CommandManager;
@@ -21,8 +17,4 @@ internal class Service
     [PluginService] public static GameGui GameGui { get; private set; } = null!;
     [PluginService] public static DalamudCondition Condition { get; private set; } = null!;
     [PluginService] public static DataManager DataManager { get; private set; } = null!;
-    [PluginService] public static ObjectTable ObjectTable { get; private set; } = null!;
-    [PluginService] public static Framework Framework { get; private set; } = null!;
-    [PluginService] public static ToastGui Toast { get; private set;} = null!;
-    [PluginService] public static AetheryteList AetheryteList { get; private set;} = null!;
 }
