4becc8da4e3b0cac0475a89da50781b903fd7bfe
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-04-08 23:47:27 -0700

    Remove Setting<T> update strings

--- Hooking/Delegates.cs
+++ Hooking/Delegates.cs
@@ -2,25 +2,14 @@
 
 namespace KamiLib.Hooking;
 
-public static class Delegates
+public static unsafe class Delegates
 {
-    public static unsafe class Addon
-    {
-        public delegate nint OnSetup(AtkUnitBase* addon, int valueCount, AtkValue* values);
-        public delegate void Draw(AtkUnitBase* addon);
-        public delegate byte OnRefresh(AtkUnitBase* addon, int valueCount, AtkValue* values);
-        public delegate void Finalize(AtkUnitBase* addon);
-        public delegate byte Update(AtkUnitBase* addon);
-    }
+    public delegate nint AddonOnSetup(AtkUnitBase* addon, int valueCount, AtkValue* values);
+    public delegate void AddonDraw(AtkUnitBase* addon);
+    public delegate byte AddonOnRefresh(AtkUnitBase* addon, int valueCount, AtkValue* values);
+    public delegate void AddonFinalize(AtkUnitBase* addon);
+    public delegate byte AddonUpdate(AtkUnitBase* addon);
 
-    public static unsafe class Agent
-    {
-        public delegate void Show(AgentInterface* agent);
-        public delegate nint ReceiveEvent(AgentInterface* agent, nint rawData, AtkValue* args, uint argCount, ulong sender);
-    }
-
-    public static unsafe class Other
-    {
-        public delegate void* GoldSaucerUpdate(void* a1, byte* a2, uint a3, ushort a4, void* a5, int* data, byte eventID);
-    }
+    public delegate void AgentShow(AgentInterface* agent);
+    public delegate nint AgentReceiveEvent(AgentInterface* agent, nint rawData, AtkValue* args, uint argCount, ulong sender);
 }
