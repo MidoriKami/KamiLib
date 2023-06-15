e0c0e11631341bbb4e76cd24e38ac6b7666549cc
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-06-15 14:14:55 -0700

    Add Reflection Helper

---
+++ Utilities/Reflection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace KamiLib.Utilities;
+
+public static class Reflection
+{
+    public static IEnumerable<T> ActivateOfType<T>() => Assembly.GetCallingAssembly()
+        .GetTypes()
+        .Where(type => type.IsSubclassOf(typeof(T)))
+        .Where(type => !type.IsAbstract)
+        .Select(type => (T?) Activator.CreateInstance(type))
+        .OfType<T>();
+
+    public static IEnumerable<T> ActivateOfInterface<T>() => Assembly.GetCallingAssembly()
+        .GetTypes()
+        .Where(type => type.GetInterfaces().Contains(typeof(T)))
+        .Where(type => !type.IsAbstract)
+        .Select(type => (T?) Activator.CreateInstance(type))
+        .OfType<T>();
+}
