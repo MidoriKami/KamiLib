63a48b8ad8d5f07fd5a451fea7bb74bbd74416d9
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-06-15 14:15:06 -0700

    Add IntCounter ConfigOption

---
+++ AutomaticUserInterface/Configuration/IntCounterConfigOption.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using ImGuiNET;
+
+namespace KamiLib.AutomaticUserInterface;
+
+public class IntCounterConfigOption : RightLabeledTabledDrawableAttribute
+{
+    public IntCounterConfigOption(string? label, string category, int group) : base(label, category, group) { }
+    
+    protected override void DrawLeftColumn(object obj, FieldInfo field, Action? saveAction = null)
+    {
+        var intValue = GetValue<int>(obj, field);
+        
+        if (ImGui.InputInt($"##{field.Name}", ref intValue, 1))
+        {
+            SetValue(obj, field, intValue);
+            saveAction?.Invoke();
+        }
+    }
+}
