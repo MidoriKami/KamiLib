6dc87fb871a4a0eda0877deade6ca09d8d083833
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-06-16 13:25:25 -0700

    Add HelpText to IntCounter

--- AutomaticUserInterface/Configuration/IntCounterConfigOption.cs
+++ AutomaticUserInterface/Configuration/IntCounterConfigOption.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Reflection;
+using Dalamud.Interface.Components;
 using ImGuiNET;
 
 namespace KamiLib.AutomaticUserInterface;
 
 public class IntCounterConfigOption : RightLabeledTabledDrawableAttribute
 {
-    public IntCounterConfigOption(string? label, string category, int group) : base(label, category, group) { }
+    private readonly string? helpTextKey;
+    private string HelpText => TryGetLocalizedString(helpTextKey);
+
+    public IntCounterConfigOption(string? label, string category, int group, string? helpText = null) : base(label, category, group)
+    {
+        helpTextKey = helpText;
+    }
     
     protected override void DrawLeftColumn(object obj, FieldInfo field, Action? saveAction = null)
     {
@@ -17,5 +24,7 @@ public class IntCounterConfigOption : RightLabeledTabledDrawableAttribute
             SetValue(obj, field, intValue);
             saveAction?.Invoke();
         }
+        
+        if(helpTextKey is not null) ImGuiComponents.HelpMarker(HelpText);
     }
 }
