0bf19c76087739500840675e145bf9811251e796
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-06-16 21:28:09 -0700

    IntCounterConfigOption.cs
    Add options to show or hide step buttons

--- AutomaticUserInterface/Configuration/IntCounterConfigOption.cs
+++ AutomaticUserInterface/Configuration/IntCounterConfigOption.cs
@@ -10,16 +10,25 @@ public class IntCounterConfigOption : RightLabeledTabledDrawableAttribute
     private readonly string? helpTextKey;
     private string HelpText => TryGetLocalizedString(helpTextKey);
 
+    private readonly bool showStep;
+
     public IntCounterConfigOption(string? label, string category, int group, string? helpText = null) : base(label, category, group)
     {
         helpTextKey = helpText;
+        showStep = true;
+    }
+    
+    public IntCounterConfigOption(string? label, string category, int group, bool showStepButtons, string? helpText = null) : base(label, category, group)
+    {
+        helpTextKey = helpText;
+        showStep = showStepButtons;
     }
     
     protected override void DrawLeftColumn(object obj, FieldInfo field, Action? saveAction = null)
     {
         var intValue = GetValue<int>(obj, field);
         
-        if (ImGui.InputInt($"##{field.Name}", ref intValue, 1))
+        if (ImGui.InputInt($"##{field.Name}", ref intValue, showStep ? 1 : 0))
         {
             SetValue(obj, field, intValue);
             saveAction?.Invoke();
