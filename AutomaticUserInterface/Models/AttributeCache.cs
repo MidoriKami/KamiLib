f71390685ee6798be7390173089b07e76a4a9c70
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-06-26 18:44:36 -0700

    Remove unused usings

--- AutomaticUserInterface/Models/AttributeCache.cs
+++ AutomaticUserInterface/Models/AttributeCache.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 using System.Reflection;
-using Dalamud.Logging;
 
 namespace KamiLib.AutomaticUserInterface.Models;
 
