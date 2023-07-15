da90f6a6b076357e45cdb0c1f2a42d9f5532b073
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-07-14 20:12:09 -0700

    First basic prototype

--- Service.cs
+++ Service.cs
@@ -1,20 +1,18 @@
-﻿using Dalamud.Data;
-using Dalamud.Game.ClientState;
+﻿using Dalamud.Game.ClientState.Conditions;
 using Dalamud.Game.Gui;
 using Dalamud.IoC;
 using Dalamud.Plugin;
-using DalamudCommandManager = Dalamud.Game.Command.CommandManager;
-using DalamudCondition = Dalamud.Game.ClientState.Conditions.Condition;
+using Dalamud.Plugin.Services;
 
 namespace KamiLib;
 
 internal class Service
 {
     [PluginService] public static DalamudPluginInterface PluginInterface { get; private set; } = null!;
-    [PluginService] public static DalamudCommandManager Commands { get; private set; } = null!;
-    [PluginService] public static ClientState ClientState { get; private set; } = null!;
+    [PluginService] public static ICommandManager Commands { get; private set; } = null!;
+    [PluginService] public static IClientState ClientState { get; private set; } = null!;
     [PluginService] public static ChatGui Chat { get; private set; } = null!;
-    [PluginService] public static GameGui GameGui { get; private set; } = null!;
-    [PluginService] public static DalamudCondition Condition { get; private set; } = null!;
-    [PluginService] public static DataManager DataManager { get; private set; } = null!;
+    [PluginService] public static IGameGui GameGui { get; private set; } = null!;
+    [PluginService] public static Condition Condition { get; private set; } = null!;
+    [PluginService] public static IDataManager DataManager { get; private set; } = null!;
 }
