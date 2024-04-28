7cb8d3ae04c8b5cf8d7d83ab0f8c05df7c965ab2
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2024-04-28 09:42:36 -0700

    Fix ClientStateHelpers name to ClientStateExtensions.cs

--- Classes/ClientStateExtensions.cs
+++ Classes/ClientStateExtensions.cs
@@ -2,7 +2,7 @@
 
 namespace KamiLib.Classes;
 
-public static class ClientStateHelpers {
+public static class ClientStateExtensions {
     public static bool IsLoggedInNotPvP(this IClientState clientState, bool excludeDen = false) {
         if (clientState is { IsLoggedIn: false }) return false;
         if (clientState is { IsPvP: true, IsPvPExcludingDen: false } && excludeDen) return false;
