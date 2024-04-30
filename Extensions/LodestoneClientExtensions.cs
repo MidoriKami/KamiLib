eeedfd5dd8954f7ea8d9db43577c69231cfa64e6
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2024-04-29 20:23:42 -0700

    Add Exception Safety

--- Extensions/LodestoneClientExtensions.cs
+++ Extensions/LodestoneClientExtensions.cs
@@ -11,25 +11,30 @@ namespace KamiLib.Classes;
 
 public static class LodestoneClientExtensions {
     internal static async Task<LodestoneCharacter?> TryGetLodestoneCharacter(this LodestoneClient client, DalamudPluginInterface pluginInterface, CharacterConfiguration character) {
-        // If lodestone id is null, try and fetch it by searching for name and world.
-        if (character.LodestoneId is null) {
-            var searchResponse = await client.SearchCharacter(new CharacterSearchQuery {
-                CharacterName = character.CharacterName,
-                World = character.CharacterWorld,
-            });
+        try {
+            // If lodestone id is null, try and fetch it by searching for name and world.
+            if (character.LodestoneId is null) {
+                var searchResponse = await client.SearchCharacter(new CharacterSearchQuery {
+                    CharacterName = character.CharacterName,
+                    World = character.CharacterWorld,
+                });
 
-            character.LodestoneId = searchResponse
-                ?.Results
-                .FirstOrDefault(entry => string.Equals(entry.Name, character.CharacterName, StringComparison.OrdinalIgnoreCase))
-                ?.Id;
+                character.LodestoneId = searchResponse
+                    ?.Results
+                    .FirstOrDefault(entry => string.Equals(entry.Name, character.CharacterName, StringComparison.OrdinalIgnoreCase))
+                    ?.Id;
             
-            pluginInterface.SaveCharacterFile(character.ContentId, "System.config.json", character);
-        }
+                pluginInterface.SaveCharacterFile(character.ContentId, "System.config.json", character);
+            }
 
-        // If it is still null, then we couldn't find it.
-        if (character.LodestoneId is null) return null;
+            // If it is still null, then we couldn't find it.
+            if (character.LodestoneId is null) return null;
         
-        // Use lodestoneId to get character.
-        return await client.GetCharacter(character.LodestoneId);
+            // Use lodestoneId to get character.
+            return await client.GetCharacter(character.LodestoneId);
+        }
+        catch (Exception e) {
+            throw new Exception($"Exception trying to get lodestone information for {character.CharacterName}.", e);
+        }
     }
 }
--- Extensions/NetStoneExtensions.cs
+++ Extensions/NetStoneExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Dalamud.Interface.Internal;
@@ -11,39 +12,44 @@ namespace KamiLib.Classes;
 
 public static class NetStoneExtensions {
     public static async Task<IDalamudTextureWrap?> TryGetProfilePicture(HttpClient httpClient, LodestoneClient lodestoneClient, DalamudPluginInterface pluginInterface, ITextureProvider textureProvider, CharacterConfiguration characterConfiguration) {
-        // We had some error while loading character configuration and don't know what character this is.
-        if (characterConfiguration.ContentId is 0) return null;
+        try {
+            // We had some error while loading character configuration and don't know what character this is.
+            if (characterConfiguration.ContentId is 0) return null;
 
-        var profilePictureFileInfo = pluginInterface.GetProfilePictureFileInfo(characterConfiguration);
+            var profilePictureFileInfo = pluginInterface.GetProfilePictureFileInfo(characterConfiguration);
         
-        // If we already have a "profile.png" picture in this characters directory, simply load it.
-        if (profilePictureFileInfo is { Exists: true } && !characterConfiguration.PurgeProfilePicture) {
-            return textureProvider.GetTextureFromFile(profilePictureFileInfo);
-        }
-        // Else we need to try and load it from Lodestone.
-        else {
-            // Try and get the LodestoneCharacter information that matches our name and world
-            if (await lodestoneClient.TryGetLodestoneCharacter(pluginInterface, characterConfiguration) is { } lodestoneCharacter) {
-                if (await httpClient.GetAsync(lodestoneCharacter.Portrait) is { } lodestoneRequestResponse) {
-                    await using (var mediaStream = await lodestoneRequestResponse.Content.ReadAsStreamAsync()) {
-                        await using (var fileStream = new FileStream(profilePictureFileInfo.FullName, FileMode.Create)) {
-                            await mediaStream.CopyToAsync(fileStream);
+            // If we already have a "profile.png" picture in this characters directory, simply load it.
+            if (profilePictureFileInfo is { Exists: true } && !characterConfiguration.PurgeProfilePicture) {
+                return textureProvider.GetTextureFromFile(profilePictureFileInfo);
+            }
+            // Else we need to try and load it from Lodestone.
+            else {
+                // Try and get the LodestoneCharacter information that matches our name and world
+                if (await lodestoneClient.TryGetLodestoneCharacter(pluginInterface, characterConfiguration) is { } lodestoneCharacter) {
+                    if (await httpClient.GetAsync(lodestoneCharacter.Portrait) is { } lodestoneRequestResponse) {
+                        await using (var mediaStream = await lodestoneRequestResponse.Content.ReadAsStreamAsync()) {
+                            await using (var fileStream = new FileStream(profilePictureFileInfo.FullName, FileMode.Create)) {
+                                await mediaStream.CopyToAsync(fileStream);
+                            }
                         }
+                    
+                        profilePictureFileInfo.Refresh();
+                    
+                        // Saving of "Profile.png" should be complete here, load the image.
+                        return profilePictureFileInfo is { Exists: false } ? null : textureProvider.GetTextureFromFile(profilePictureFileInfo);
                     }
-                    
-                    profilePictureFileInfo.Refresh();
-                    
-                    // Saving of "Profile.png" should be complete here, load the image.
-                    return profilePictureFileInfo is { Exists: false } ? null : textureProvider.GetTextureFromFile(profilePictureFileInfo);
+                }
+                // Else, unrecoverable, we don't have a "profile.png" and we couldn't load it from Lodestone.
+                else {
+                    return null;
                 }
             }
-            // Else, unrecoverable, we don't have a "profile.png" and we couldn't load it from Lodestone.
-            else {
-                return null;
-            }
-        }
         
-        // Somehow slipped into unknown territory, return null.
-        return null;
+            // Somehow slipped into unknown territory, return null.
+            return null;
+        }
+        catch (Exception e) {
+            throw new Exception($"Exception trying to load profile picture for {characterConfiguration.CharacterName}.", e);
+        }
     }
 }
