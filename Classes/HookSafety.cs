d03821b819e52b97fffeb5b841bb7113b2eaa485
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2024-05-05 11:36:36 -0700

    Add more helper classes

---
+++ Classes/HookSafety.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using Dalamud.Plugin.Services;
+
+namespace KamiLib.Classes;
+
+public static class HookSafety {
+    public static void ExecuteSafe(Action action, IPluginLog log, string? message = null) {
+        try {
+            action();
+        }
+        catch (Exception exception) {
+            var trace = new StackTrace().GetFrame(1);
+            var callingAssembly = Assembly.GetCallingAssembly().GetName().Name;
+
+            if (trace is not null) {
+                var callingClass = trace.GetMethod()?.DeclaringType;
+                var callingName = trace.GetMethod()?.Name;
+
+                log.Error($"Exception Source: {callingAssembly} :: {callingClass} :: {callingName}");
+            }
+
+            log.Error(exception, message ?? "Caught Exception Safely");
+        }
+    }
+}
