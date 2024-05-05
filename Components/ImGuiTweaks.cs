c97aa45f692691269c24adad01d4c9d9913ef9cf
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2024-05-04 19:30:43 -0700

    Add ColorEditWithDefault

---
+++ Components/ImGuiTweaks.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+using ImGuiNET;
+
+namespace KamiLib.TabBar;
+
+public static class ImGuiTweaks {
+    public static bool ColorEditWithDefault(string label, ref Vector4 color, Vector4 defaultColor) {
+        var valueChanged = ImGui.ColorEdit4($"##{label}", ref color, ImGuiColorEditFlags.AlphaPreviewHalf | ImGuiColorEditFlags.NoInputs);
+
+        ImGui.SameLine();
+        
+        if (ImGui.Button($"Default##{label}")) {
+            color = defaultColor;
+        }
+
+        ImGui.SameLine();
+
+        ImGui.TextUnformatted(label);
+        
+        return valueChanged;
+    }
+}
