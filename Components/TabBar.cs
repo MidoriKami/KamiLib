1570205cb535b7d3f48867b7c000b40801e693eb
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2024-05-07 18:17:20 -0700

    Add option to disallow scrolling in tab child

--- Components/TabBar.cs
+++ Components/TabBar.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Numerics;
 using Dalamud.Interface.Utility.Raii;
+using ImGuiNET;
 
 namespace KamiLib.TabBar;
 
@@ -9,7 +11,7 @@ public interface ITabItem {
     void Draw();
 }
 
-public class TabBar(string name, List<ITabItem> tabs) {
+public class TabBar(string name, List<ITabItem> tabs, bool allowScrolling = true) {
     public void Draw() {
         using var tabBar = ImRaii.TabBar(name);
         foreach (var tab in tabs) {
@@ -17,7 +19,7 @@ public class TabBar(string name, List<ITabItem> tabs) {
             using var tabItem = ImRaii.TabItem(tab.Name);
             
             if (tabItem) {
-                using var child = ImRaii.Child($"tab_{tab.Name}_child");
+                using var child = ImRaii.Child($"tab_{tab.Name}_child", new Vector2(0.0f, 0.0f), false, allowScrolling ? ImGuiWindowFlags.None : ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse);
                 tab.Draw();
             }
         }
