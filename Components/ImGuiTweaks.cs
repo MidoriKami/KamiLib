4cc266fc040b486ea0c4a29ea56453adef947f80
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2024-05-07 18:17:09 -0700

    Add IconButtonWithSize

--- Components/ImGuiTweaks.cs
+++ Components/ImGuiTweaks.cs
@@ -1,4 +1,6 @@
 ﻿using System.Numerics;
+using Dalamud.Interface;
+using Dalamud.Interface.Utility.Raii;
 using ImGuiNET;
 
 namespace KamiLib.TabBar;
@@ -18,5 +20,20 @@ public static class ImGuiTweaks {
         ImGui.TextUnformatted(label);
         
         return valueChanged;
+    }
+
+    public static bool IconButtonWithSize(FontAwesomeIcon icon, string id, Vector2 size, string? tooltip = null) {
+        using var imRaiiId = ImRaii.PushId(id);
+        bool result;
+
+        using (var _ = ImRaii.PushFont(UiBuilder.IconFont)) {
+            result = ImGui.Button($"{icon.ToIconString()}", size);
+        }
+
+        if (ImGui.IsItemHovered(ImGuiHoveredFlags.AllowWhenDisabled) && tooltip is not null) {
+            ImGui.SetTooltip(tooltip);
+        }
+
+        return result;
     }
 }
