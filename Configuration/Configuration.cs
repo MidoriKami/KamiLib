ae6fb0418b8fca0c0d18b1b75da778ba2cefa7a4
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2024-05-18 20:39:06 -0700

    Rework configuration loading and saving a little, simplify down exception handling, and properly log errors

--- Configuration/Configuration.cs
+++ Configuration/Configuration.cs
@@ -4,15 +4,106 @@ using System.IO;
 using System.Linq;
 using System.Text.Json;
 using Dalamud.Plugin;
-using KamiLib.Configuration;
+using KamiLib.Classes;
 
-namespace KamiLib.Classes;
+namespace KamiLib.Configuration;
 
 public static class Configuration {
     public static readonly JsonSerializerOptions SerializerOptions = new() {
         WriteIndented = true,
         IncludeFields = true,
     };
+    
+    /// <summary>
+    /// Load a configuration file for a specific character.
+    /// </summary>
+    /// <param name="pluginInterface">Dalamud Plugin Interface used to find the correct directory.</param>
+    /// <param name="contentId">ContentId of the desired character file.</param>
+    /// <param name="fileName">Specific name of the file you wish to load.</param>
+    /// <param name="createFunc">A function to create that file if loading fails.</param>
+    /// <typeparam name="T">Type of file to load, needed for serialization.</typeparam>
+    /// <returns>Either a loaded config file, or a new'd file of type T.</returns>
+    public static T LoadCharacterFile<T>(this DalamudPluginInterface pluginInterface, ulong contentId, string fileName, Func<T> createFunc) where T : new() 
+        => LoadFile(pluginInterface, pluginInterface.GetCharacterFileInfo(contentId, fileName).FullName, createFunc);
+
+    /// <summary>
+    /// Load a configuration file from the plugin specific config directory.
+    /// </summary>
+    /// <param name="pluginInterface">Dalamud Plugin Interface used to find the correct directory.</param>
+    /// <param name="fileName">Specific name of the file you wish you load.</param>
+    /// <param name="createFunc">A function to create the file if loading fails.</param>
+    /// <typeparam name="T">Type of file to load, needed for serialization.</typeparam>
+    /// <returns>Either a loaded config file, or a new'd file of type T.</returns>
+    public static T LoadConfigFile<T>(this DalamudPluginInterface pluginInterface, string fileName, Func<T> createFunc) where T : new() 
+        => LoadFile(pluginInterface, Path.Combine(pluginInterface.ConfigDirectory.FullName, fileName), createFunc);
+
+    /// <summary>
+    /// Save a configuration file for a specific character.
+    /// </summary>
+    /// <param name="pluginInterface">Dalamud Plugin Interface used to find the correct directory.</param>
+    /// <param name="contentId">ContentId of the desired character file.</param>
+    /// <param name="fileName">Specific name of the file you wish to save.</param>
+    /// <param name="file">The object to write to a file.</param>
+    /// <typeparam name="T">Type of file to load, needed for serialization.</typeparam>
+    public static void SaveCharacterFile<T>(this DalamudPluginInterface pluginInterface, ulong contentId, string fileName, T file) 
+        => SaveFile(pluginInterface, pluginInterface.GetCharacterFileInfo(contentId, fileName).FullName, file);
+
+    /// <summary>
+    /// Save a configuration file to the plugin specific config directory.
+    /// </summary>
+    /// <param name="pluginInterface">Dalamud Plugin Interface used to find the correct directory.</param>
+    /// <param name="fileName">Specific name of the file you wish to save.</param>
+    /// <param name="file">The object to write to a file.</param>
+    /// <typeparam name="T">Type of file to load, needed for serialization.</typeparam>
+    public static void SaveConfigFile<T>(this DalamudPluginInterface pluginInterface, string fileName, T file)
+        => SaveFile(pluginInterface, Path.Combine(pluginInterface.GetPluginConfigDirectory(), fileName), file);
+
+    private static T LoadFile<T>(DalamudPluginInterface pluginInterface, string filePath, Func<T> createFunc) where T : new() {
+        var fileInfo = new FileInfo(filePath);
+        if (fileInfo is { Exists: true }) {
+            try {
+                var fileText = File.ReadAllText(fileInfo.FullName);
+                var dataObject = JsonSerializer.Deserialize<T>(fileText, SerializerOptions);
+
+                // If deserialize result is null, create a new instance instead and save it.
+                if (dataObject is null) {
+                    dataObject = createFunc();
+                    SaveFile(pluginInterface, filePath, dataObject);
+                }
+                
+                return dataObject;
+            }
+            catch (Exception e) {
+                // If there is any kind of error loading the file, generate a new one instead and save it.
+                var localLog = pluginInterface.Create<LogWrapper>();
+                localLog?.Log.Error($"Error trying to load file {filePath}, creating a new one instead.", e);
+                
+                SaveFile(pluginInterface, filePath, createFunc());
+            }
+        }
+        
+        return createFunc();
+    }
+
+    private static void SaveFile<T>(DalamudPluginInterface pluginInterface, string filePath, T file) {
+        try {
+            var fileText = JsonSerializer.Serialize(file, SerializerOptions);
+            Dalamud.Utility.Util.WriteAllTextSafe(filePath, fileText);
+        }
+        catch (Exception e) {
+            var localLog = pluginInterface.Create<LogWrapper>();
+            localLog?.Log.Error($"Error trying to save file {filePath}", e);
+        }
+    } 
+
+    internal static DirectoryInfo GetCharacterDirectoryInfo(this DalamudPluginInterface pluginInterface, ulong contentId) 
+        => new(Path.Combine(pluginInterface.ConfigDirectory.FullName, contentId.ToString()));
+
+    internal static FileInfo GetCharacterFileInfo(this DalamudPluginInterface pluginInterface, ulong contentId, string fileName) 
+        => new(Path.Combine(pluginInterface.GetCharacterDirectoryInfo(contentId).FullName, fileName));
+
+    internal static IEnumerable<CharacterConfiguration> GetAllCharacterConfigurations(this DalamudPluginInterface pluginInterface) 
+        => pluginInterface.GetAllCharacterContentIds().Select(pluginInterface.LoadCharacterConfiguration);
 
     private static CharacterConfiguration LoadCharacterConfiguration(this DalamudPluginInterface pluginInterface, ulong contentId) {
         var loadedConfiguration = pluginInterface.LoadCharacterFile(contentId, "System.config.json", () => CreateNewCharacterConfig(contentId));
@@ -25,51 +116,6 @@ public static class Configuration {
         return loadedConfiguration;
     }
     
-    public static T LoadCharacterFile<T>(this DalamudPluginInterface pluginInterface, ulong contentId, string fileName, Func<T> createFunc) where T : new() {
-        var fileInfo = pluginInterface.GetCharacterFileInfo(contentId, fileName);
-        if (fileInfo is { Exists: true }) {
-            try {
-                var fileText = File.ReadAllText(fileInfo.FullName);
-                var dataObject = JsonSerializer.Deserialize<T>(fileText, SerializerOptions);
-
-                // If deserialize result is null, create a new instance instead and save it.
-                if (dataObject is null) {
-                    dataObject = createFunc();
-                    pluginInterface.SaveCharacterFile(contentId, fileName, dataObject);
-                }
-                
-                return dataObject;
-            }
-            catch (Exception) {
-                // If there is any kind of error loading the file, generate a new one instead and save it.
-                var dataObject = createFunc();
-                pluginInterface.SaveCharacterFile(contentId, fileName, dataObject);
-            }
-        }
-        
-        return createFunc();
-    }
-
-    public static void SaveCharacterFile<T>(this DalamudPluginInterface pluginInterface, ulong contentId, string fileName, T file) {
-        try {
-            var fileInfo = pluginInterface.GetCharacterFileInfo(contentId, fileName);
-            var fileText = JsonSerializer.Serialize(file, SerializerOptions);
-            Dalamud.Utility.Util.WriteAllTextSafe(fileInfo.FullName, fileText);
-        }
-        catch (Exception e) {
-            throw new Exception($"Error while trying to save file for CID:{contentId}, {fileName}.", e);
-        }
-    }
-    
-    public static DirectoryInfo GetCharacterDirectoryInfo(this DalamudPluginInterface pluginInterface, ulong contentId) 
-        => new(Path.Combine(pluginInterface.ConfigDirectory.FullName, contentId.ToString()));
-
-    public static FileInfo GetCharacterFileInfo(this DalamudPluginInterface pluginInterface, ulong contentId, string fileName) 
-        => new(Path.Combine(pluginInterface.GetCharacterDirectoryInfo(contentId).FullName, fileName));
-
-    internal static IEnumerable<CharacterConfiguration> GetAllCharacterConfigurations(this DalamudPluginInterface pluginInterface) 
-        => pluginInterface.GetAllCharacterContentIds().Select(pluginInterface.LoadCharacterConfiguration);
-
     private static IEnumerable<ulong> GetAllCharacterContentIds(this DalamudPluginInterface pluginInterface) {
         if (pluginInterface.ConfigDirectory is { Exists: true } directoryInfo) {
             foreach (var directory in directoryInfo.EnumerateDirectories()) {
