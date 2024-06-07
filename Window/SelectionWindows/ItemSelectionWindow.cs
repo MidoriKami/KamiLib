5fc0b8dcca5c43f96a3a2b9e299aeefc1233d37f
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2024-06-06 21:41:19 -0700

    Add ItemSelection Windows (HQ, NQ, Collectable)

---
+++ Window/SelectionWindows/ItemSelectionWindow.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using Dalamud.Interface.Textures;
+using Dalamud.Interface.Utility;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+using Dalamud.Utility;
+using ImGuiNET;
+using Lumina.Excel.GeneratedSheets;
+
+namespace KamiLib.Window.SelectionWindows;
+
+
+public class ItemSelectionWindow : SelectionWindowBase<Item> {
+    [PluginService] private ITextureProvider TextureProvider { get; set; } = null!;
+    [PluginService] private IDataManager DataManager { get; set; } = null!;
+
+    public ItemSelectionWindow(DalamudPluginInterface pluginInterface) : base(new Vector2(300.0f, 600.0f), false) {
+        pluginInterface.Inject(this);
+
+        SelectionOptions = DataManager
+            .GetExcelSheet<Item>()!
+            .Where(item => !item.Name.ToString().IsNullOrEmpty())
+            .OrderBy(item => item.Name.ToString())
+            .ToList();
+    }
+    
+    protected override bool AllowMultiSelect => true;
+    protected override float SelectionHeight => 30.0f * ImGuiHelpers.GlobalScale;
+    
+    protected override void DrawSelection(Item option) {
+        if (TextureProvider.GetFromGameIcon(new GameIconLookup {
+                IconId = option.Icon,
+                ItemHq = false,
+                HiRes = true,
+            }) is { } texture) {
+            ImGui.Image(texture.GetWrapOrEmpty().ImGuiHandle, ImGuiHelpers.ScaledVector2(30.0f, 30.0f));
+            ImGui.SameLine();
+            ImGui.SetCursorPosY(ImGui.GetCursorPos().Y + 5.0f * ImGuiHelpers.GlobalScale);
+            ImGui.Text(option.Name);
+        }
+    }
+    
+    protected override bool FilterResults(Item option, string filter)
+        => option.Name.ToString().Contains(filter, StringComparison.OrdinalIgnoreCase);
+}
