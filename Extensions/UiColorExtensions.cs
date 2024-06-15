7a5a1e91ae3af92f1ea65b8017a4585ccd2f4e54
MidoriKami <mi_kami@outlook.com>
2024-06-15 13:55:41 -0700

    Add UiColorSelectionWindow.cs

---
+++ Extensions/UiColorExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+using Lumina.Excel.GeneratedSheets;
+
+namespace KamiLib.Extensions;
+
+public static class UiColorExtensions {
+	public static Vector4 Foreground(this UIColor color)
+		=> ConvertToVector4(color.UIForeground);
+
+	public static Vector4 Glow(this UIColor color)
+		=> ConvertToVector4(color.UIGlow);
+	
+	private static Vector4 ConvertToVector4(uint color) {
+		var r = (byte)(color >> 24);
+		var g = (byte)(color >> 16);
+		var b = (byte)(color >> 8);
+		var a = (byte)color;
+
+		return new Vector4(r / 255.0f, g / 255.0f, b / 255.0f, a / 255.0f);
+	}
+}
---
+++ Window/SelectionWindows/UiColorSelectionWindow.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Numerics;
+using Dalamud.Interface.Utility;
+using Dalamud.Plugin.Services;
+using ImGuiNET;
+using KamiLib.Extensions;
+using Lumina.Excel.GeneratedSheets;
+
+namespace KamiLib.Window.SelectionWindows;
+
+public class UiColorSelectionWindow: SelectionWindowBase<UIColor> {
+	protected override bool AllowMultiSelect => false;
+	
+	protected override float SelectionHeight => 32.0f * ImGuiHelpers.GlobalScale;
+	
+	protected override bool ShowFilter => false;
+
+	public UiColorSelectionWindow(IDataManager dataManager) : base(new Vector2(250.0f, 400.0f))
+		=> SelectionOptions = dataManager.GetExcelSheet<UIColor>()!.ToList();
+
+	protected override void DrawSelection(UIColor option) {
+		var cursorStart = ImGui.GetCursorScreenPos();
+		
+		ImGui.GetWindowDrawList().AddRectFilled(cursorStart + new Vector2(3.0f, 3.0f), cursorStart + new Vector2(ImGui.GetContentRegionAvail().X, 32.0f * ImGuiHelpers.GlobalScale) - new Vector2(3.0f, 3.0f), ImGui.GetColorU32(option.Foreground()));
+		ImGui.SetCursorScreenPos(cursorStart);
+		ImGuiHelpers.ScaledDummy(32.0f);
+	}
+
+	protected override bool FilterResults(UIColor option, string filter)
+		=> true;
+}
