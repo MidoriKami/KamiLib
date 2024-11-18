bbf5f6319115f8d340fc34a3604c201c915cb87e
MidoriKami <mi_kami@outlook.com>
2024-11-17 17:19:26 -0800

    Add teleport method

---
+++ Classes/Teleporter.cs
@@ -0,0 +1,22 @@
+﻿using Dalamud.Game.Text;
+using Dalamud.Game.Text.SeStringHandling;
+using Dalamud.Plugin.Services;
+using FFXIVClientStructs.FFXIV.Client.Game.UI;
+using Lumina.Excel.Sheets;
+
+namespace KamiLib.Classes;
+
+public static unsafe class Teleporter {
+	public static void Teleport(IDataManager dataManager, IChatGui chatGui, uint aetheryteId, string pluginName) {
+		var aetheryte = dataManager.GetExcelSheet<Aetheryte>().GetRow(aetheryteId);
+		Telepo.Instance()->Teleport(aetheryteId, 0);
+		chatGui.Print(new XivChatEntry {
+			Message = new SeStringBuilder()
+				.AddUiForeground($"[{pluginName}] ", 45)
+				.AddUiForeground("[Teleport] ", 62)
+				.AddText("Teleporting to ")
+				.AddUiForeground(aetheryte.PlaceName.Value.Name.ExtractText(), 576)
+				.Build(),
+		});
+	}
+}
