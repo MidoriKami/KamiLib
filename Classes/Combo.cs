882f3af5e919724a2c1426638cda7aef0247ab11
MidoriKami <mi_kami@outlook.com>
2024-12-03 11:37:16 -0800

    Create Combo Element

---
+++ Classes/Combo.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Dalamud.Interface.Utility.Raii;
+using ImGuiNET;
+
+namespace KamiLib.Classes;
+
+public interface IComboItem {
+	string Label { get; }
+
+	void Draw();
+}
+
+public class Combo(string name, List<IComboItem> items) {
+	private IComboItem selectedItem = items[0];
+	
+	public void Draw() {
+		if (items.Count != 0) {
+			ImGui.SetNextItemWidth(ImGui.GetContentRegionAvail().X);
+			using (var combo = ImRaii.Combo($"##{name}",selectedItem.Label, ImGuiComboFlags.HeightLarge)) {
+				if (combo) {
+					foreach (var item in items) {
+						if (ImGui.Selectable(item.Label, selectedItem == item)) {
+							selectedItem = item;
+						}
+					
+					}
+				}
+			}
+
+			using (var child = ImRaii.Child("selectedItem", ImGui.GetContentRegionAvail())) {
+				if (child) {
+					selectedItem.Draw();
+				}
+			}
+		}
+		else {
+			ImGui.Text("KamiLib.Combo Error, no items.");
+		}
+	}
+}
