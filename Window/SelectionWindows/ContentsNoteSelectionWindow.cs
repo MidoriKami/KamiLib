d9d48cc7f2e0dd89314243408656d0c2d857cdb0
MidoriKami <mi_kami@outlook.com>
2025-05-07 21:33:32 -0700

    Switch ContentsNoteSelectionWindow.cs to IconItemSelectionWindow

--- Window/SelectionWindows/ContentsNoteSelectionWindow.cs
+++ Window/SelectionWindows/ContentsNoteSelectionWindow.cs
@@ -3,18 +3,17 @@ using System.Drawing;
 using System.Linq;
 using System.Numerics;
 using Dalamud.Interface;
-using Dalamud.Interface.Textures;
 using Dalamud.Interface.Utility;
-using Dalamud.Interface.Utility.Raii;
 using Dalamud.IoC;
 using Dalamud.Plugin;
 using Dalamud.Plugin.Services;
 using ImGuiNET;
+using KamiLib.Extensions;
 using Lumina.Excel.Sheets;
 
 namespace KamiLib.Window.SelectionWindows;
 
-public class ContentsNoteSelectionWindow : SelectionWindowBase<ContentsNote> {
+public class ContentsNoteSelectionWindow : IconItemSelectionWindow<ContentsNote> {
 	[PluginService] private IDataManager DataManager { get; set; } = null!;
 	[PluginService] private ITextureProvider TextureProvider { get; set; } = null!;
 
@@ -28,30 +27,12 @@ public class ContentsNoteSelectionWindow : SelectionWindowBase<ContentsNote> {
 	
 	protected override bool AllowMultiSelect => true;
 	protected override float SelectionHeight => 64.0f * ImGuiHelpers.GlobalScale;
-	
-	protected override void DrawSelection(ContentsNote option) {
-		using (var imageContainer = ImRaii.Child("image_container", ImGuiHelpers.ScaledVector2(64.0f, ImGui.GetContentRegionAvail().Y), false, ImGuiWindowFlags.NoInputs)) {
-			if (!imageContainer) return;
+	protected override float IconWidth => 64.0f * ImGuiHelpers.GlobalScale;
 
-			DrawIcon(option);
-		}
-		
-		ImGui.SameLine();
-		
-		using (var textContainer = ImRaii.Child("text_container", ImGui.GetContentRegionAvail(), false, ImGuiWindowFlags.NoInputs)) {
-			if (!textContainer) return;
-			
-			DrawText(option);
-		}
-	}
+	protected override void DrawIcon(ContentsNote option)
+		=> TextureProvider.DrawGameIcon(option.Icon);
 
-	private void DrawIcon(ContentsNote option) {
-		var icon = TextureProvider.GetFromGameIcon(new GameIconLookup((uint) option.Icon)).GetWrapOrEmpty();
-		
-		ImGui.Image(icon.ImGuiHandle, icon.Size * ImGuiHelpers.GlobalScale);
-	}
-	
-	private static void DrawText(ContentsNote option) {
+	protected override void DrawText(ContentsNote option) {
 		ImGui.Text(option.Name.ExtractText());
 		ImGui.Spacing();
 		ImGuiHelpers.SafeTextColoredWrapped(KnownColor.Gray.Vector().Lighten(0.20f), option.Description.ExtractText());
