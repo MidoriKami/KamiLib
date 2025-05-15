1658639e56503399b35184fbdce915aad2eb767c
MidoriKami <mi_kami@outlook.com>
2025-05-14 22:56:33 -0700

    Add Vector Extensions

---
+++ Extensions/VectorExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Numerics;
+using Dalamud.Interface;
+
+namespace KamiLib.Extensions;
+
+public static class VectorExtensions {
+	public static Vector3 AsNormalizedUnitVector3(this Vector3 vector)
+		=> vector.AsVector4().NormalizeToUnitRange().AsVector3();
+}
