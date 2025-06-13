4afe3f345cf90c442b9d1e86710903d216be5ddb
MidoriKami <mi_kami@outlook.com>
2025-06-13 14:24:52 -0700

    Add ByteColor extensions

---
+++ Extensions/ByteColorExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Numerics;
+using FFXIVClientStructs.FFXIV.Client.Graphics;
+
+namespace KamiLib.Extensions;
+
+public static class ByteColorExtensions {
+	public static Vector4 ToVector4(this ByteColor color)
+		=> new Vector4(color.R, color.G, color.B, color.A) / 255.0f;
+}
