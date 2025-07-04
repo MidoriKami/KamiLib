d2a9c48e23c81512748e5d686662d902ed5b6a17
MidoriKami <mi_kami@outlook.com>
2025-07-03 21:06:15 -0700

    Use helper to get Addon Pointer

---
+++ Extensions/AddonArgsExtensions.cs
@@ -0,0 +1,8 @@
+﻿using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
+
+namespace KamiLib.Extensions;
+
+public static unsafe class AddonArgsExtensions {
+	public static T* GetAddon<T>(this AddonArgs args) where T : unmanaged
+		=> (T*) args.Addon;
+}
