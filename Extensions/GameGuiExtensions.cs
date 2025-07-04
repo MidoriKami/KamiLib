520f4c6bc8e6e34ce46d173ae319948af723f5aa
MidoriKami <mi_kami@outlook.com>
2025-07-04 15:37:28 -0700

    Add GameGui Extension to get Addon without name

--- Extensions/GameGuiExtensions.cs
+++ Extensions/GameGuiExtensions.cs
@@ -1,8 +1,20 @@
-﻿using Dalamud.Plugin.Services;
+﻿using System.Linq;
+using System.Reflection;
+using Dalamud.Plugin.Services;
+using FFXIVClientStructs.Attributes;
 
 namespace KamiLib.Extensions;
 
 public static class GameGuiExtensions {
     public static unsafe T* GetAddonByName<T>(this IGameGui gameGui, string addonName) where T : unmanaged
         => (T*) gameGui.GetAddonByName(addonName);
+
+    public static unsafe T* GetAddon<T>(this IGameGui gameGui) where T : unmanaged {
+        var type = typeof(T);
+        var attribute = type.GetCustomAttributes().OfType<AddonAttribute>().FirstOrDefault();
+        var addonName = attribute?.AddonIdentifiers.FirstOrDefault();
+        
+        if (addonName is null) return null;
+        return (T*) gameGui.GetAddonByName(addonName);
+    }
 }
